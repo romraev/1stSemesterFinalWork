accf8ed86a3a3613621aeac4e3b365e7fb859417
Romraev <romraev@yandex.ru>
2023-04-23 19:30:04 +0300

    string array initialization

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿string[] FillArray(int size)
+{
+    string[] strings = new string[size];
+
+    for(int i=0; i<strings.Length; i++)
+    {
+        Console.Write("Enter string: ");
+        var text = Console.ReadLine(); // Выдавал ошибку, что может быть null (правда так и не понял как), пришлось пойти таким путём, хотя и без того всё работает.
+        if(text is null) strings[i] = String.Empty;
+        else strings[i] = text;
+    }
+    return strings;
+}
