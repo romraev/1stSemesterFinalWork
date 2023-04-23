786a3ebd7395a7623b7fd0969faad4728385ed41
Romraev <romraev@yandex.ru>
2023-04-23 20:56:11 +0300

    data input and methods use

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 {
     string[] strings = new string[size];
 
-    for(int i=0; i<strings.Length; i++)
+    for (int i = 0; i < strings.Length; i++)
     {
         Console.Write("Enter string: ");
         var text = Console.ReadLine(); // Выдавал ошибку, что может быть null (правда так и не понял как), пришлось пойти таким путём, хотя и без того всё работает.
-        if(text is null) strings[i] = String.Empty;
+        if (text is null) strings[i] = String.Empty;
         else strings[i] = text;
     }
     return strings;
@@ -15,19 +15,26 @@
 string[] SelectStrings(string[] array)
 {
     string[] newStrings = new string[array.Length];
-    for(int i=0; i<array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if(array[i].Length < 4) newStrings[i] = array[i];
+        if (array[i].Length < 4) newStrings[i] = array[i];
         else newStrings[i] = String.Empty;
     }
     return newStrings;
 }
 
-void WriteArray(string[]array)
+void WriteArray(string[] array)
 {
     Console.Write("[");
-    for(int i=0; i<array.Length; i++)
-        if(array[i] != String.Empty) Console.Write("\"" + array[i] + "\", ");
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] != String.Empty) Console.Write("\"" + array[i] + "\", ");
     Console.Write("\b" + "\b" + "]");
     Console.WriteLine();
 }
+
+Console.Write("Enter integer number: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+string[] initialStrings = FillArray(num);
+WriteArray(initialStrings);
+WriteArray(SelectStrings(initialStrings));
